8f8b7684a37e3047fc791aa47cdc391f8cbff48a
Brian Donohue <brian.donohue89@gmail.com>
2015-06-25 11:42:30 -0400

    One day reprise: Revert "Kill anonbot"
    This reverts commit 90867842fc08b0d9b8643607b2649dc0e37c7cd9.

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ from flask import Flask, request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-#SLACK_OUTGOING_WEBHOOK_TOKEN = os.environ.get('SLACK_OUTGOING_WEBHOOK_TOKEN', None)
-#SLACK_INCOMING_WEBHOOK_URL = os.environ['SLACK_INCOMING_WEBHOOK_URL']
+SLACK_OUTGOING_WEBHOOK_TOKEN = os.environ.get('SLACK_OUTGOING_WEBHOOK_TOKEN', None)
+SLACK_INCOMING_WEBHOOK_URL = os.environ['SLACK_INCOMING_WEBHOOK_URL']
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    #token = request.form.get('token')
-    #if not SLACK_OUTGOING_WEBHOOK_TOKEN or token == SLACK_OUTGOING_WEBHOOK_TOKEN:
-    #    params = {
-    #            'username': 'anonbot',
-    #            'icon_emoji': ':ghost:',
-    #            'text': request.form.get('text'),
-    #            'channel': '#%s' % request.form.get('channel_name')
-    #    }
-    #    try:
-    #        response = requests.post(SLACK_INCOMING_WEBHOOK_URL, data=json.dumps(params))
-    #    except Exception, e:
-    #        return 'Posting to Slack failed: %s' % str(e)
-    return 'anonbot is ded'
+    token = request.form.get('token')
+    if not SLACK_OUTGOING_WEBHOOK_TOKEN or token == SLACK_OUTGOING_WEBHOOK_TOKEN:
+        params = {
+                'username': 'anonbot',
+                'icon_emoji': ':ghost:',
+                'text': request.form.get('text'),
+                'channel': '#%s' % request.form.get('channel_name')
+        }
+        try:
+            response = requests.post(SLACK_INCOMING_WEBHOOK_URL, data=json.dumps(params))
+        except Exception, e:
+            return 'Posting to Slack failed: %s' % str(e)
+    return ''
 
